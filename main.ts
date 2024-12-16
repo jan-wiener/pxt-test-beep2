58fb83e6c2cb2ab41d7147126f9230ae4a12be33
Jan Wiener <jan.wiener.024@pslib.cz>
2024-12-16 10:14:03 +0100

    Počáteční soubory projektu MakeCode

---
+++ main.ts
@@ -0,0 +1,29 @@
+
+
+
+function catchA() {
+
+    if (input.buttonIsPressed(Button.A) && lastTimePressedA == 0) {
+        music.playTone(440, 50)
+        return 1
+    } else if (input.buttonIsPressed(Button.A)) {
+        return 1
+    } else {
+        return 0
+    }
+   
+    
+}
+
+let lastTimePressedA: number = 0
+basic.forever(function() {
+
+
+    lastTimePressedA = catchA()
+    
+    console.log(lastTimePressedA)
+    console.log(control.millis())
+
+
+
+})
